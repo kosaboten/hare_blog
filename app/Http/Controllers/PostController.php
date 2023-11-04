b78c5c9a0b3c88128438f1a10e86c1e50cf6c0b4
kosaboten <saboten.system@gmail.com>
2023-11-04 17:24:46 +0900

    リファクタリング

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
         $post->user_id = $request->user()->id; //ログインしているユーザのIDを取り出す
         
         $file = $request->file('image');
-        $post->image = date('YmdHis') . '_' . $file->getClientOriginalName();
+        $post->image = self::createFileName($file);
 
         // トランザクション開始
         DB::beginTransaction();
@@ -74,21 +74,65 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('posts.edit', compact('post'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(StorePostRequest $request, string $id)
+public function update(UpdatePostRequest $request, string $id)
     {
-    }
+        $post = Post::find($id);
 
+        if ($request->user()->cannot('update', $post)) {
+            return redirect()->route('posts.show', $post)
+                ->withErrors('自分の記事以外は更新できません');
+        }
+
+        $file = $request->file('image');
+        if ($file) {
+            $delete_file_path = 'images/posts/' . $post->image;
+            $post->image = self::createFileName($file);
+        }
+        $post->fill($request->all());
+
+        DB::beginTransaction();
+        try {
+            $post->save();
+
+            if ($file) {
+                // 画像アップロード
+                if (!Storage::putFileAs('images/posts', $file, $post->image)) {
+                    throw new \Exception('画像ファイルの保存に失敗しました。');
+                }
+
+                // 画像削除
+                if (!Storage::delete($delete_file_path)) {
+                    Storage::delete('images/posts/' . $post->image);
+                    throw new \Exception('画像ファイルの削除に失敗しました');
+                }
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.show', $post)
+            ->with('notice', '記事を更新しました');
+    }
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
         //
+    }
+
+    private static function createFileName($file){
+        return date('YmdHis') . '_' . $file->getClientOriginalName();
     }
 }
